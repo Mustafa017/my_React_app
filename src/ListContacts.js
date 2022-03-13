742186ace41c9b76f627eb7eb11a33684ec51dcf
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-13 13:36:10 -0700

    feat: Using double curly braces and template literals

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -6,7 +6,18 @@ class ListContact extends Component {
     return (
       <ol className="contact-list">
         {this.props.contacts.map((contact) => (
-          <li key={contact.id}>{contact.name}</li>
+          <li key={contact.id} className="contact-list-item">
+            {/* double curly braces to inline the style attribute. Template literals parse the url */}
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${contact.avatarURL})` }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className="contact-remove">remove</button>
+          </li>
         ))}
       </ol>
     );

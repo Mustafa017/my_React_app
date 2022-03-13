b7e1a87ada39a1ed5e916811291cd4e403e01f7c
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-13 11:39:39 -0700

    feat: Remove Default Files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
-import React, { Component } from "react";
-// import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return (
-      <ol>
-        {people.map((person, index) => (
-          <li key={index}>{person.name}</li>
-        ))}
-      </ol>
-    );
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ContactList
-          contacts={[
-            { name: "Mustafa" },
-            { name: "Stephen" },
-            { name: "Abdul" },
-          ]}
-        />
-        <ContactList
-          contacts={[{ name: "Alex" }, { name: "Beatrice" }, { name: "Janet" }]}
-        />
-      </div>
-    );
-  }
+function App() {
+  return <div>Hello World</div>;
 }
 
 export default App;

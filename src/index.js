818e3a5cbc8f9fc908e6ecbd1ecbb1847126dffb
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-13 11:33:21 -0700

    feat: Intro to components

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-class ContactList extends React.Component {
-  render() {
-    const people = [
-      { name: "Mustafa" },
-      { name: "Stephen" },
-      { name: "Abdul" },
-    ];
-
-    return (
-      <ol>
-        {people.map((person, index) => (
-          <li key={index}>{person.name}</li>
-        ))}
-      </ol>
-    );
-  }
-}
-
-ReactDOM.render(<ContactList />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

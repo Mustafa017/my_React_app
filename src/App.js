6d35f320376b5da07a2d26ac1de7dd4ff2c8918c
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-18 14:36:58 -0500

    Feat: set contacts array as state in App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from "react";
 import ListContact from "./ListContacts";
 
-const contacts = [
-  {
-    id: "ryan",
-    name: "Ryan Florence",
-    email: "ryan@reacttraining.com",
-    avatarURL: "http://localhost:5001/ryan.jpg",
-  },
-  {
-    id: "michael",
-    name: "Michael Jackson",
-    email: "michael@reacttraining.com",
-    avatarURL: "http://localhost:5001/michael.jpg",
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    email: "tyler@reacttraining.com",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-];
-
 class App extends Component {
+  // move the contacts array into the state so that react can track changes to its contents.
+  // e.g when an entry is deleted
+  state = {
+    contacts: [
+      {
+        id: "ryan",
+        name: "Ryan Florence",
+        email: "ryan@reacttraining.com",
+        avatarURL: "http://localhost:5001/ryan.jpg",
+      },
+      {
+        id: "michael",
+        name: "Michael Jackson",
+        email: "michael@reacttraining.com",
+        avatarURL: "http://localhost:5001/michael.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        email: "tyler@reacttraining.com",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
   render() {
-    return <ListContact contacts={contacts}></ListContact>;
+    return <ListContact contacts={this.state.contacts}></ListContact>;
   }
 }
 

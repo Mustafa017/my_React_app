47d0b6832af71cc4aa5c0f38f36c97d7ef1eca94
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-18 14:19:11 -0500

    Feat: Create a stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListContact extends Component {
-  render() {
-    console.log("props", this.props);
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className="contact-list-item">
-            {/* double curly braces to inline the style attribute. Template literals parse the url */}
-            <div
-              className="contact-avatar"
-              style={{ backgroundImage: `url(${contact.avatarURL})` }}
-            ></div>
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className="contact-remove">remove</button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
+// creating a stateless function component.
+// Note that the first parameter is props and we don't use the `this` keyword.
+function ListContact(props) {
+  return (
+    <ol className="contact-list">
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          {/* double curly braces to inline the style attribute. Template literals parse the url */}
+          <div
+            className="contact-avatar"
+            style={{ backgroundImage: `url(${contact.avatarURL})` }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className="contact-remove">remove</button>
+        </li>
+      ))}
+    </ol>
+  );
 }
 
 export default ListContact;

347b3a1af02bb58d7b74a124b393193836a489c0
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-22 10:27:34 -0500

    Feat: Display Queried Contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import sortBy from "sort-by";
+import escapeRegExp from "escape-string-regexp";
 
 // change to a class component because we need to introduce a state to our component
 class ListContact extends Component {
@@ -15,13 +17,25 @@ class ListContact extends Component {
   updateQuery = (query) => {
     // Remember the form inputs are saved in the DOM.
     // We are not updating the state based on previous state, therefore we can pass an
-    // object instead of a function to setState. The object gets merged to the current state.
+    // object instead of a function to setState. The new state is merged to the component's internal state.
     this.setState({
       query: query.trim(),
     });
   };
 
   render() {
+    let showingContacts;
+
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), "i");
+      showingContacts = this.props.contacts.filter((contact) =>
+        match.test(contact.name)
+      );
+    } else {
+      showingContacts = this.props.contacts;
+    }
+
+    showingContacts.sort(sortBy("name"));
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
@@ -31,7 +45,7 @@ class ListContact extends Component {
             type="text"
             placeholder="Search Contacts"
             value={this.state.query}
-            // 1. whenever a value changes, the onchange calls updateQuery passing it the text.
+            // 1. whenever a value changes, the onchange event listener invokes updateQuery passing it the text.
             // 2. Then updateQuery function updates the state (i.e the query property)
             // 3. The Input field is updated with the value in the state.
             onChange={(event) => {
@@ -41,7 +55,7 @@ class ListContact extends Component {
         </div>
         {JSON.stringify(this.state)}
         <ol className="contact-list">
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
               {/* double curly braces to inline the style attribute. Template literals parse the url */}
               <div

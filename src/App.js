352a1cfb94988deb77634618bf3c4c6fb58f13d5
Mustafa Khamisi <mustafangure95@gmail.com>
2022-03-22 14:09:10 -0500

    Feat: Use API to Fetch Remote contacts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import React, { Component } from "react";
 import ListContact from "./ListContacts";
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
   // move the contacts array into the state so that react can track changes to its contents.
   // e.g when an entry is deleted
   state = {
-    contacts: [
-      {
-        id: "ryan",
-        name: "Ryan Florence",
-        email: "ryan@reacttraining.com",
-        avatarURL: "http://localhost:5001/ryan.jpg",
-      },
-      {
-        id: "michael",
-        name: "Michael Jackson",
-        email: "michael@reacttraining.com",
-        avatarURL: "http://localhost:5001/michael.jpg",
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        email: "tyler@reacttraining.com",
-        avatarURL: "http://localhost:5001/tyler.jpg",
-      },
-    ],
+    contacts: [],
   };
-
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts });
+    });
+  }
   // passing a function to setState instead of an object.
   // This is done if your new state depends on the current state.
   removeContact = (contact) => {
